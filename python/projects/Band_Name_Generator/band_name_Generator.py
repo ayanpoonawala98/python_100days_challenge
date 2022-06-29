7102634af47d853270d9f09902a4e00f072c3b73
ayanpoonawala98 <rayankhan9822@gmail.com>
2022-06-29 18:26:29 +0530

    day1.1

---
+++ python/projects/Band_Name_Generator/band_name_Generator.py
@@ -0,0 +1,8 @@
+city = ""
+pet = ""
+
+city = input("what\'s name of the city you grew up in?\n")
+
+pet = input("what\'s your pet\'s name?\n")
+
+print("Your band name could be "+ city + " " + pet)
